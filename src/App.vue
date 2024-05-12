9fcaf62e727a3dc7fe201d175978213a8296a928
kaishi <zerokara.imasara@gmail.com>
2024-05-13 06:05:14 +0900

    second

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Events</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
       <h2>Events For Good</h2>
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,42 +1,20 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService.js'
 import EventCard from '@/components/EventCard.vue'
-import { ref } from 'vue'
 
-const events = ref([
-  {
-    id: 12344,
-    category: 'animal welfare-1',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28,2022',
-    time: '13:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 12332,
-    category: 'animal welfare-2',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28,2022',
-    time: '13:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 124,
-    category: 'animal welfare-3',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28,2022',
-    time: '13:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  }
-])
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      console.log('ok!')
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
